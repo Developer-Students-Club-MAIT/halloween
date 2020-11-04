0fd7a52b672561c86569804666dbe5367c24ec00
roopamgarg <roopamg777@gmail.com>
2020-11-04 13:21:30 +0530

    Updates

---
+++ precache-manifest.c745493e33614bacc0aa0b5d788088c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77142c44f30dc20ec50ac214b4aa8542",
+    "url": "/helloween/index.html"
+  },
+  {
+    "revision": "1631c16d4aebacdb64bc",
+    "url": "/helloween/static/css/main.3bac404e.chunk.css"
+  },
+  {
+    "revision": "01c10de40ec7b5bfeb1a",
+    "url": "/helloween/static/js/2.6a7beca3.chunk.js"
+  },
+  {
+    "revision": "bd16ffa8a645b42adcb7fc65d4c790f8",
+    "url": "/helloween/static/js/2.6a7beca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1631c16d4aebacdb64bc",
+    "url": "/helloween/static/js/main.1ee6c2ae.chunk.js"
+  },
+  {
+    "revision": "5b5b27bb6c50d841c429",
+    "url": "/helloween/static/js/runtime-main.182b5b80.js"
+  },
+  {
+    "revision": "1a35d010c00094a9ea038e037b08d8dc",
+    "url": "/helloween/static/media/fortune_teller.1a35d010.jpg"
+  }
+]);

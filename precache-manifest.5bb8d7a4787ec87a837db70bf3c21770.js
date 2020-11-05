39a65093979d9b1e907f992a256cdf3860858d33
roopamgarg <roopamg777@gmail.com>
2020-11-05 14:00:49 +0530

    Updates

---
+++ precache-manifest.5bb8d7a4787ec87a837db70bf3c21770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e4b47fc2fb166556f11b287285d14d",
+    "url": "/helloween/index.html"
+  },
+  {
+    "revision": "4487448946c61c3359d9",
+    "url": "/helloween/static/css/main.3bac404e.chunk.css"
+  },
+  {
+    "revision": "1dcb0944f4b491a309ff",
+    "url": "/helloween/static/js/2.996fdccb.chunk.js"
+  },
+  {
+    "revision": "bd16ffa8a645b42adcb7fc65d4c790f8",
+    "url": "/helloween/static/js/2.996fdccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4487448946c61c3359d9",
+    "url": "/helloween/static/js/main.79fcf52c.chunk.js"
+  },
+  {
+    "revision": "5b5b27bb6c50d841c429",
+    "url": "/helloween/static/js/runtime-main.182b5b80.js"
+  },
+  {
+    "revision": "1a35d010c00094a9ea038e037b08d8dc",
+    "url": "/helloween/static/media/fortune_teller.1a35d010.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa54cb3833105ca9d38685662f6c0ef2.js"
+  "/helloween/precache-manifest.5bb8d7a4787ec87a837db70bf3c21770.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/helloween/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

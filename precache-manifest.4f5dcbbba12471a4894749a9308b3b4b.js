564077f4b1cae3aad3c98e42c33ff60a8b928d24
roopamgarg <roopamg777@gmail.com>
2020-11-05 18:19:36 +0530

    Updates

---
+++ precache-manifest.4f5dcbbba12471a4894749a9308b3b4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581386f8dac1d926f2addc78def086ea",
+    "url": "/helloween/index.html"
+  },
+  {
+    "revision": "7ec7d36b9258ff4e7f28",
+    "url": "/helloween/static/css/main.3bac404e.chunk.css"
+  },
+  {
+    "revision": "1e56d94459349dccef8f",
+    "url": "/helloween/static/js/2.1763ecde.chunk.js"
+  },
+  {
+    "revision": "bd16ffa8a645b42adcb7fc65d4c790f8",
+    "url": "/helloween/static/js/2.1763ecde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec7d36b9258ff4e7f28",
+    "url": "/helloween/static/js/main.cffb98e4.chunk.js"
+  },
+  {
+    "revision": "69b786429000161b43ce",
+    "url": "/helloween/static/js/runtime-main.ff55b0c4.js"
+  },
+  {
+    "revision": "1a35d010c00094a9ea038e037b08d8dc",
+    "url": "/helloween/static/media/fortune_teller.1a35d010.jpg"
+  },
+  {
+    "revision": "f0f95b816e6237a28be6fbfa489dfb93",
+    "url": "/helloween/static/media/horror.f0f95b81.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.928224f10c5c9951808aba54179acac9.js"
+  "/helloween/precache-manifest.4f5dcbbba12471a4894749a9308b3b4b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/helloween/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
